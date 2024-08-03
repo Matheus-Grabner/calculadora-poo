6b941e37fd44068ed67a1e5e7637e7232c006099
Matheus Grabner <101662483+Matheus-Grabner@users.noreply.github.com>
2024-08-03 14:49:33 -0300

    JS
    Final.

--- js/scripts.js
+++ js/scripts.js
@@ -22,45 +22,136 @@ class Calculator {
         }
     }
 
-    btnPress() {
+    //método de soma
+    sum(n1, n2) {
+        return parseFloat(n1) + parseFloat(n2);
+    }
 
-        let input = this.textContent;
-        let upperValue = calc.upperValue.textContent;
-        //verificar se tem só números
+    //método de subtração
+    subtraction(n1, n2) {
+        return parseFloat(n1) - parseFloat(n2);
+    }
+
+    //método de multiplicação
+    multiplication(n1, n2) {
+        return parseFloat(n1) * parseFloat(n2);
+    }
+
+    //método de divisão
+    division(n1, n2) {
+        return parseFloat(n1) / parseFloat(n2);
+    }
+
+    //atualiza valores
+    refreshValues(total) {
+        this.upperValue.textContent = total;
+        this.resultValue.textContent = total;
+    }
+
+    //resolve a operação
+    resolution() {
+
+        //explode uma string em um array
+        let upperValueArray = (this.upperValue.textContent).split(' ');
+        //resultado da operação
+        let result = 0;
+
+        for (let i = 0; i <= upperValueArray.length; i++) {
+
+            let operation = 0;
+            let actualItem = upperValueArray[i];
+
+            //faz a multiplicação
+            if (actualItem == 'x') {
+                result = calc.multiplication(upperValueArray[i - 1], upperValueArray[i + 1]);
+                operation = 1;
+                //faz a divisão
+            } else if (actualItem == '/') {
+                result = calc.division(upperValueArray[i - 1], upperValueArray[i + 1]);
+                operation = 1;
+                //checa se o array ainda tem multiplicação e divisão a ser feita
+            } else if (!upperValueArray.includes('x') && !upperValueArray.includes('/')) {
+                //soma e subtração
+                if (actualItem == '+') {
+                    result = calc.sum(upperValueArray[i - 1], upperValueArray[i + 1]);
+                    operation = 1;
+                    //faz a divisão
+                } else if (actualItem == '-') {
+                    result = calc.subtraction(upperValueArray[i - 1], upperValueArray[i + 1]);
+                    operation = 1;
+                }
+            }
+
+            //atualiza valores do array para a próxima iteração
+            if (operation) {
+                //indice anterior no resultado da operação
+                upperValueArray[i - 1] = result;
+                //remove os itens já utilizados para a operação
+                upperValueArray.splice(i, 2);
+                //atualizar o valor do índice
+                i = 0;
+            }
+
+        }
+
+        if (result) {
+            calc.reset = 1;
+        }
+
+        //atualizar os totais
+        calc.refreshValues(result);
+    }
+
+    btnPress(event) {
+        let input = event.target.textContent;
+        let upperValue = this.upperValue.textContent;
+        // verificar se tem só números
         var reg = new RegExp('^\\d+$');
 
-        //ativa método de limpar o display
+        //se precisar resetar, limpa o display
+        if (calc.reset && reg.test(input)) {
+            upperValue = '0';
+        }
+
+        //limpa a prop de reset
+        calc.reset = 0;
+
+        // ativa método de limpar o display
         if (input == 'AC') {
             calc.clearValues();
+        } else if (input == '=') {
+            calc.resolution();
         } else {
-            //checa se precisa adicionar ou não
+            // checa se precisa adicionar ou não
             if (calc.checkLastDigit(input, upperValue, reg)) {
                 return false;
             }
 
-            //adiciona espaços aos operadores
+            // adiciona espaços aos operadores
             if (!reg.test(input)) {
                 input = ` ${input} `;
             }
 
             if (upperValue == '0') {
-                calc.upperValue.textContent = input;
+                if (reg.test(input)) {
+                    calc.upperValue.textContent = input;
+                }
             } else {
                 calc.upperValue.textContent += input;
             }
-
         }
     }
-
 }
 
-//start obj
-let calc = new Calculator;
+// start obj
+let calc = new Calculator();
 
-//start btns
+// start btns
 let buttons = document.querySelectorAll('.btn');
 
-//map all buttons
+// map all buttons
 for (let i = 0; buttons.length > i; i++) {
-    buttons[i].addEventListener('click', calc.btnPress);
+    buttons[i].addEventListener('click', function (event) {
+        calc.btnPress(event);
+    });
 }
